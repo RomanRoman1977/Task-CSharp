6ae4514bbc5811390897195df15fb1119c5150ec
Roman <kormakoff.roma@gmail.com>
2022-08-04 21:05:12 +0300

    Четные от 1 до N

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,6 +1,8 @@
-﻿Console.Write("Введите значение N: ");
+﻿// Console.Write("Введите значение N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
-for(int i = 0; i <= N; i++){
-    
-}
+for (int i = 0; i <= N; i = i + 2)
+    Console.WriteLine("Число {0}", i);
