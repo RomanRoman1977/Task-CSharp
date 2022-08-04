d687332c9dadb9e41b152a03e3a9e8b81c38c831
Roman <kormakoff.roma@gmail.com>
2022-08-04 16:43:10 +0300

    Скопировал Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите значение № 1: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение № 2: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num_1 > num_2)
+{
+    Console.WriteLine($"{num_1} больше {num_2}");
+}
+else
+{
+    Console.WriteLine($"{num_2} больше {num_1}");
+}
+if (num_1 == num_2)
+{
+    Console.WriteLine("Значения равны");
+}
