100e2303418c799d5a8f5e00704a3701394b0565
Roman <kormakoff.roma@gmail.com>
2022-08-04 18:43:35 +0300

    Четное

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите значение: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Значение четное");
+}
+else
+{
+    Console.WriteLine($"Значение не четное");
+}
