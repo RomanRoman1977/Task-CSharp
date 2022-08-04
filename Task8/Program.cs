9982dd02a2f2de4f9e1e6ff8b6f74fe8235d9471
Roman <kormakoff.roma@gmail.com>
2022-08-04 21:14:14 +0300

    Четные от 1 до N

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,8 +1,8 @@
-﻿// Console.Write("Введите значение N: ");
-// int N = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите значение N: ");
+﻿Console.Write("Введите значение N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 0; i <= N; i = i + 2)
+    if(i=0)
+        continue;
     Console.WriteLine("Число {0}", i);
+    
