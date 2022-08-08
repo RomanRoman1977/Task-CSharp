517b5ece585c976146e726b76764cc41e0576b6d
Roman <kormakoff.roma@gmail.com>
2022-08-08 11:22:01 +0300

    Генерируем число

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11. Напишите программу, которая выводит случайное
+//  трёхзначное число и удаляет вторую цифру этого 
+//  числа.
+//  456 -> 46
+//  782 -> 72
+//  918 -> 98
+
+int DeleteMiddleDigit (int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+int number = new Random().Next(100, 1000);
+
+int res = DeleteMiddleDigit(number);
+Console.WriteLine($"Сгенерированное число {number}. Обработанное число -> {res}");
+
