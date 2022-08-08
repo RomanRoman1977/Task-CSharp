93cd1ee6770b8ee75ad87abbe4459e00f549cbe6
Roman <kormakoff.roma@gmail.com>
2022-08-08 10:59:57 +0300

    Task9

---
+++ Task9/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int number = new Random().Next(10, 100); //10, 99+1
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+// if (firstDigit == secondDigit) Console.WriteLine("Числа равны");
+// else
+// {
+//int max = firstDigit;
+//if (secondDigit > max) max = secondDigit ? firstDigit : secondDigit
+
+// Console.WriteLine($"Наибольшая цифра числа {nunber} =>");
+// Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+//Console.WriteLine($"Наибольшая цифра числа {nunber} => {max} ");
+// }
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit == secondDigit) return -1;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;    
+}
+
+int maxDigit = MaxDigit(number);
+string result = maxDigit != -1 ? maxDigit.ToString() : "Цыфры одинаковые";
+Console.WriteLine($"Наибольшая цифра числа {number} => {result} ");
